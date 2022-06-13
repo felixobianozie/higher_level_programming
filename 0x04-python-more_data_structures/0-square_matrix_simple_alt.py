56746c30ceda573114f934860b1d8a28c552bc16
felixobianozie <felixobianozie@gmail.com>
2022-06-13 13:04:52 +0000

    2nd modifications to 0-square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple_alt.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple_alt.py
@@ -14,16 +14,31 @@ def square_matrix_simple(matrix=[]):
     a new matrix:Same size as matrix. Original matrix is not modified
 
     Approach:
-    Using list comprehension, for every item in matrix called x (which is
-    assumed would be a list), return x. Then within each returned x perform
-    y**2 for every item therein called y.
+    1.Create new list variable to hold result of all subsequent evaluations
+    2.For each item (a list) in matrix,
+    3.Create temp. list variable for each new item from matrix after x^2
+    4.Use map function to perform exponentiation per item from matrix
+    5.Append modified item from matrix to new list variable
+    6.Clear temporary list variable from memory
+    7.Return the newly generated list
 
     Author:
     Felix Obianozie
     '''
 
-    # Using list comprehension
-    newList = [[y**2 for y in x]for x in matrix]
+    # 1
+    newList = []
 
-    # Return the newly generated list
+    # 2
+    for i in matrix:
+        # 3 & 4
+        tempList = list(map(lambda x: x**2, i))
+
+        # 5
+        newList.append(tempList)
+
+    # 6
+    del tempList
+
+    # 7
     return newList
