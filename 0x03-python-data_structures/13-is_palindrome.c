765abaca02d798e705a0e27eb8acb1098db660f5
felixobianozie <felixobianozie@gmail.com>
2022-09-01 23:39:04 +0000

    Palindrome checker completed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,116 +1,128 @@
 #include "lists.h"
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 int digit_count(int);
-int digit_pos(int);
-float digit_dec(int);
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: head pointer to singly linked list
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Authour: Felix Obianozie
  */
 
 int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
-    	int nodes = 0;
+	int nodes = 0;
 	int limit = 0;
-	float value1 = 0;
-	float value2 = 0;
-	float tmp = 0;
+	int tmp = 1;
 	int i = 0;
-	float plc_val = 1;
-	
+	int j = 0;
+
+	long double value1 = 0;
+	long double value2 = 0;
+
 	/*Empty list is palindrome*/
 	if (*head == NULL)
 		return (1);
-	
+
 	/*Count nodes*/
 	while (current != NULL)
 	{
 		current = current->next;
 		nodes++;
 	}
-	
+	current = *head;
+
 	/*List with one node is palindrome*/
 	if (nodes == 1)
 		return (1);
-	
-	/*Palindrome even case*/
+
+	/*Palindrome in an even case*/
 	if (nodes % 2 == 0)
 	{
 		limit = nodes / 2;
-		current = *head;
-		for(i = 0; i < limit; i++)
+		for (i = 0; i < limit; i++)
 		{
-			value1 = (value1 * digit_pos(digit_count(current->n))) + current->n;
-printf("%d\n", digit_pos(digit_count(current->n)));
+			j = 0;
+			tmp = digit_count(current->n);
+			while (j < tmp)
+			{
+				value1 = value1 * 10;
+				j++;
+			}
+			value1 = value1 + current->n;
 			current = current->next;
 		}
 
-		for(i = 0; current != NULL; i++)
+		tmp = 1;
+		for (i = 0; current != NULL; i++)
 		{
-			value2 = (value2 / digit_pos(digit_count(current->n))) + current->n;
-			if (i > 0)
-				plc_val = plc_val * digit_dec(digit_count(current->n));
-printf("tmp: %f; plc_val:%f\n", value2, plc_val);
+			while (value2 >= 1)
+			{
+				value2 = value2 / 10;
+				if (i > 0)
+					tmp = tmp * 10;
+			}
+			value2 = value2 + current->n;
 			current = current->next;
 		}
-		value2 = (value2 / 0.001);
+		value2 = value2 * tmp;
 	}
 
-printf("Value1: %f; Value2: %f; plc_val: %f\n", value1, value2, plc_val);
+	/*Palindrome in an odd case*/
+	if (nodes % 2 != 0)
+	{
+		limit = (nodes / 2) + 1;
+		for (i = 0; i < limit; i++)
+		{
+			j = 0;
+			tmp = digit_count(current->n);
+			while (j < tmp)
+			{
+				value1 = value1 * 10;
+				j++;
+			}
+			value1 = value1 + current->n;
+			tmp = current->n;
+			current = current->next;
+		}
 
-digit_count(456);
+		value2 = tmp;
+		tmp = 1;
+		for (i = 0; current != NULL; i++)
+		{
+			while (value2 >= 1)
+			{
+				value2 = value2 / 10;
+				tmp = tmp * 10;
+			}
+			value2 = value2 + current->n;
+			current = current->next;
+		}
+		value2 = value2 * tmp;
+	}
 
 	if (value1 == value2)
-	{
-		printf("Shit is a palindrome\n");
-		return(1);
-	}
-
+		return (1);
 	return (0);
 }
 
+/**
+ * digit_count - estimates the number of digits of a given integer value.
+ * @value: the supposed integer value
+ *
+ * Return: number of digits of given integer.
+ */
 
 int digit_count(int value)
 {
+	int count = 0;
 
-	if ((value / 10) == 0)
-		return (1);
-	else if ((value / 100) == 0)
-		return (2);
-	else if ((value / 1000) == 0)
-		return (3);
-}
-
-int digit_pos(int count)
-{
-	int x[] = {1, 2, 3};
-	int y[] = {10, 100, 1000};
-	int i;
-
-	for (i = 0; i < 3; i++)
+	while (value >= 1)
 	{
-		if (count == x[i])
-			return (y[i]);
+		value = value / 10;
+		count++;
 	}
-	return (0);
-}
 
-float digit_dec(int count)
-{
-	int x[] = {1, 2, 3};
-	float y[] = {0.1, 0.01, 0.001};
-	int i;
-
-	for (i = 0; i < 3; i++)
-	{
-		if (count == x[i])
-			return (y[i]);
-	}
-	return (0);
+	return (count);
 }
